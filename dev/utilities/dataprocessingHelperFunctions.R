482023a6dc19ab6c61998f842e4ed1272fd04150
stressedplants <120037059+stressedplants@users.noreply.github.com>
2023-11-23 22:20:59 +0000

    Updated Network Tutorial
    Created a tutorial to help you make your first gene network

--- dev/utilities/dataprocessingHelperFunctions.R
+++ dev/utilities/dataprocessingHelperFunctions.R
@@ -2,20 +2,55 @@
 
 #' Calculating Z-scores for a matrix or data frame
 #'
+#' @description This function calculates a z-score (for each row by default).  
+#' This normalises the rows so they have a mean of zero and a standard deviation of 1,
+#' which allows for easier comparisons between 
+#'
 #' @param mat This is a numerical matrix or data frame.
 #' @param byRow This specifies whether the z-scores will be calculated for every row or every column in the matrix or data frame
 #'
 #' @return a matrix in which every row (or column, if byRow=FALSE) has a mean of 0 and a standard deviation of 1.
-#' @export
 #'
-#' @examples  
+#' @examples mat=matrix(data=c(1, 3, 4, 5, 2, 4, 5, 2), byrow = T, nrow = 2)
+#' zscore(mat)
+#' zscore(mat, byRow=FALSE)
 #' 
 zscore<-function(mat, byRow=TRUE){
   side=1
   if(!byRow){side=2}
   
-  apply(mat, side, function(i){
+  temp=apply(mat, side, function(i){
     (i-mean(i))/sd(i)
   })
   
+  if(dim(temp)[1]!=dim(mat)[1] & dim(temp)[2]!=dim(mat)[2]){
+    t(temp)
+  }else{temp}
+  
+}
+
+#' Convert output of GENIE3 into an adjacency matrix
+#'
+#' @description This takes a matrix where rows are regulators and columns are target and values are scores
+#' It converts the table to a three column layout where the score of each regulator-target pair is specified
+#' 
+#' @param network This is the network (usually the output of GENIE3)
+#' @param threshold This is the threshold of score above which the edge will be included in the final table
+#'
+#' @return a three column table, containing the regulator, the target, and the score.  Only rows with scores exceeding the threshold will be provided.
+#'
+convertToAdjacency <- function(network, threshold){
+
+  temp=unlist(sapply(rownames(network), function(i){
+    paste(i, colnames(network)[which(network[i,]>threshold)], network[i,which(network[i,]>threshold)])
+  }))
+  
+  col3=data.frame(t(sapply(temp, function(i){
+    split=strsplit(i, ' ')
+    c(split[[1]][1], split[[1]][2], split[[1]][3])
+  })))
+  
+  col3[,3]=as.numeric(col3[,3])
+  
+  col3
 }
