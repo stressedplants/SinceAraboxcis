e83a490084e30cc0a12c03f899b3286d58a406e3
AlexMarks533 <145984954+AlexMarks533@users.noreply.github.com>
2024-04-05 21:54:17 +0100

    Update Venndiagram_attempt.R
    I think I actually have done it this time.

--- dev/scripting/Venndiagram_attempt.R
+++ dev/scripting/Venndiagram_attempt.R
@@ -13,59 +13,41 @@ d12Net = GENIE3::getLinkList(net_d12_50)
 d06Net = GENIE3::getLinkList(net_d06_50)
 d03Net = GENIE3::getLinkList(net_d03_50)
 
-#(the gene network) into a sorted list of regulatory links. 
-araboxcis = read.csv('data/gboxNetwork22C.csv', header = T)
+edges_d12 = paste(d12Net[,1], d12Net[,2], sep = '_')
+edges_d06 = paste(d06Net[,1], d06Net[,2], sep = '_')
+edges_d03 = paste(d03Net[,1], d03Net[,2], sep = '_')
 
-#gets the set of unique genes in each network
-uniquegenes_d12Net = unique(c(d12Net[,1], d12Net[,2]))
-uniquegenes_d06Net = unique(c(d06Net[,1], d06Net[,2]))
-uniquegenes_d03Net = unique(c(d03Net[,1], d03Net[,2]))
-
-#Findiing all the common genes
-common_genes <- Reduce(intersect, 
-                       list(uniquegenes_d12Net, uniquegenes_d06Net, 
-                            uniquegenes_d03Net))
-
-#Creating filtered data sets where the rows only have 
-#genes that are common to all data sets
-d12Net_filtered = d12Net[which(d12Net$regulatoryGene %in% common_genes
-                              & d12Net$targetGene %in% common_genes),]
-
-d06Net_filtered = d12Net[which(d06Net$regulatoryGene %in% common_genes
-                               & d06Net$targetGene %in% common_genes),]
-
-d03Net_filtered = d12Net[which(d03Net$regulatoryGene %in% common_genes
-                               & d03Net$targetGene %in% common_genes),]
-
-#reformatting so edges can be compared(this turn the data into 1 column)
-edges_d12 = paste(d12Net_filtered[,1], d12Net_filtered[,2], sep = '_')
-edges_d06 = paste(d06Net_filtered[,1], d06Net_filtered[,2], sep = '_')
-edges_d03 = paste(d03Net_filtered[,1], d03Net_filtered[,2], sep = '_')
-
-#finding the edges in all dX using intsect
-length(intersect(intersect(edges_d12, edges_d06), edges_d03))
 common_edges <- intersect(intersect(edges_d12, edges_d06), edges_d03)
+length(common_edges)
 
-#finding the edges in all two of the three data sets using intsect
-d12_d06_common_edges <- intersect(edges_d12, edges_d06)
-#removing the edges that are in the common_edges data set
-d12_d06_common_edges <- setdiff(d12_d06_common_edges, common_edges)
-length(d12_d06_common_edges)
+exclusive_edges_d12_d06 <- setdiff(intersect(edges_d12, edges_d06), common_edges)
+length(exclusive_edges_d12_d06)
+exclusive_edges_d12_d03 <- setdiff(intersect(edges_d12, edges_d03), common_edges)
+length(exclusive_edges_d12_d03)
+exclusive_edges_d03_d06 <- setdiff(intersect(edges_d03, edges_d06), common_edges)
+length(exclusive_edges_d03_d06)
 
-#finding the edges in all two of the three data sets using intsect
-d12_d03_common_edges <- intersect(edges_d12, edges_d03)
-#removing the edges that are in the common_edges data set
-d12_d03_common_edges <- setdiff(d12_d03_common_edges, common_edges)
-length(d12_d03_common_edges)
+exclusive_edges_d12 <- setdiff(setdiff(edges_d12, edges_d06), edges_d03)
+length(exclusive_edges_d12)
+exclusive_edges_d06 <- setdiff(setdiff(edges_d06, edges_d12), edges_d03)
+length(exclusive_edges_d06)
+exclusive_edges_d03 <- setdiff(setdiff(edges_d03, edges_d06), edges_d12)
+length(exclusive_edges_d03)
 
-#finding the edges in all two of the three data sets using intsect
-d06_d03_common_edges <- intersect(edges_d06, edges_d03)
-#removing the edges that are in the common_edges data set
-d06_d03_common_edges <- setdiff(d06_d03_common_edges, common_edges)
-length(d06_d03_common_edges)
 
-#finding the edges only in each dataset
-length(setdiff(setdiff(setdiff(edges_d12, common_edges), d12_d03_common_edges),d12_d06_common_edges )) 
+d03_data <- c(length(exclusive_edges_d03), length(exclusive_edges_d03_d06), length(exclusive_edges_d12_d03), length(common_edges))
+d06_data <- c(length(exclusive_edges_d03_d06), length(exclusive_edges_d06), length(exclusive_edges_d12_d06), length(common_edges))
+d12_data <- c(length(exclusive_edges_d12_d03), length(exclusive_edges_d12_d06), length(exclusive_edges_d12), length(common_edges))
 
-length(setdiff(setdiff(setdiff(edges_d06, common_edges), d06_d03_common_edges), d12_d06_common_edges))
-length(setdiff(setdiff(setdiff(edges_d03, common_edges), d06_d03_common_edges), d12_d03_common_edges))
+Venn_matrix <- matrix(c(d03_data, d06_data, d12_data), nrow = 3, ncol = 4, byrow = TRUE, 
+                           dimnames = list(c("d03", "d06", "d12"), 
+                                           c("shared_d03", "shared_d06", "shared_d12", "common")))
+
+Venn_matrix <- cbind(Venn_matrix, rowSums(Venn_matrix))
+
+# Update column names
+colnames(Venn_matrix) <- c(colnames(Venn_matrix)[1:4], "total")
+
+# Print the updated matrix
+print(Venn_matrix)
+
